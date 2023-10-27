fe15b0ba4fbe29c5522d4ba9be5a7b193bc183e6
Mr Random <100831398+MrRandom23@users.noreply.github.com>
2023-10-27 17:23:53 +0530

    v1.1.1 Release

--- welcome.js
+++ welcome.js
@@ -1,78 +1,72 @@
-const { createCanvas, loadImage, GlobalFonts } = require('@napi-rs/canvas');
+const { createCanvas, loadImage, GlobalFonts } = require("@napi-rs/canvas");
 
+GlobalFonts.registerFromPath(
+  "node_modules/welcome-canvas/font/Caveat-VariableFont_wght.ttf",
+  "username"
+);
+GlobalFonts.registerFromPath(
+  "node_modules/welcome-canvas/font/Montserrat-ExtraLight.ttf",
+  "montserrat-extra-light"
+);
+GlobalFonts.registerFromPath(
+  "node_modules/welcome-canvas/font/EduSABeginner-VariableFont_wght.ttf",
+  "welcome"
+);
 
-GlobalFonts.registerFromPath("node_modules/welcome-canvas/font/Caveat-VariableFont_wght.ttf", "username");
-GlobalFonts.registerFromPath("node_modules/welcome-canvas/font/Montserrat-ExtraLight.ttf", "montserrat-extra-light");
-GlobalFonts.registerFromPath("node_modules/welcome-canvas/font/EduSABeginner-VariableFont_wght.ttf", "welcome");
+const imageUrls = "https://s6.imgcdn.dev/ZpIVD.png";
 
+class welcome {
+  constructor(options) {
+    this.username = options?.username;
+    this.avatar = options?.avatar;
+    this.title = options?.title;
+    this.message = options?.message;
+    this.background = options?.background;
+  }
 
-const imageUrls = [
-  'https://s6.imgcdn.dev/ZpcLw.png',
-  'https://s6.imgcdn.dev/ZpCUT.png',
-  'https://s6.imgcdn.dev/Zp4wt.png',
-  'https://s6.imgcdn.dev/ZpIVD.png',
-  'https://s6.imgcdn.dev/ZpNo9.png',
-  'https://s6.imgcdn.dev/Zpisy.png',
-  'https://s6.imgcdn.dev/Zpkx8.png',
-  'https://s6.imgcdn.dev/ZpnH2.png',
-];
+  setName(name) {
+    this.username = name;
+    return this;
+  }
 
+  setAvatar(image) {
+    this.avatar = image;
+    return this;
+  }
 
-  let currentImageIndex = 0;
+  setTitle(title) {
+    this.title = title;
+    return this;
+  }
 
+  setMessage(message) {
+    this.message = message;
+    return this;
+  }
 
-  class welcome {
-    constructor(options) {
-        this.username = options?.username;
-        this.avatar = options?.avatar;
-        this.title = options?.title;
-        this.message = options?.message;
-        this.background = options?.background;
+  setBackground(background) {
+    this.background = background;
+    return this;
+  }
+
+  async build() {
+    if (!this.username) throw new Error("Provide username to display on card");
+    if (!this.avatar) throw new Error("Provide valid avatar url of user");
+    if (!this.title) this.setTitle("WELCOME");
+    if (!this.message) throw new Error("Provide message to display on card");
+    if (!this.background) this.setBackground(imageUrls);
+
+    if (this.username.length >= 27) {
+      this.username = this.username.slice(0, 24) + "...";
     }
 
-    setName(name) {
-        this.username = name;
-        return this;
+    if (this.title.length >= 27) {
+      this.title = this.title.slice(0, 24) + "...";
     }
 
-    setAvatar(image) {
-        this.avatar = image;
-        return this;
+    if (this.message.length >= 27) {
+      this.message = this.message.slice(0, 24) + "...";
     }
-
-    setTitle(title) {
-        this.title = title;
-        return this;
-    }
-
-    setMessage(message) {
-        this.message = message;
-        return this;
-    }
-
-    setBackground(background) {
-        this.background = background
-        return this;
-    }
-
-    async build() {
-        if (!this.username) throw new Error("Provide username to display on card");
-        if (!this.avatar) throw new Error("Provide valid avatar url of user");
-        if (!this.title) this.setTitle("WELCOME");
-        if (!this.message) throw new Error("Provide message to display on card");
-        this.setBackground(imageUrls)
-
-        if(this.username.length >= 27) {
-            throw new Error(`The Username Should be Less Than or Equal To 27 Words`);
-        }
-
-        if(this.title.length >= 27) {
-            throw new Error(`The title Should be Less Than or Equal To 27 Words`);
-        }
-
-        if (this.message.length >= 27) {
-            throw new Error(`The message Should be Less Than or Equal To 27 Words`);
-        }
 
     const canvasWidth = 1280;
     const canvasHeight = 720;
@@ -81,53 +75,44 @@ const imageUrls = [
     const centerY = canvasHeight / 2;
 
     const canvas = createCanvas(canvasWidth, canvasHeight);
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
     // Load the current background image
-    const backgroundImage = await loadImage(imageUrls[currentImageIndex]);
+    const backgroundImage = await loadImage(this.background);
     ctx.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
 
-    // Increment the current image index and loop back to the first image if it's the last one
-    currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
-
-  
-   
-    const gradientImage = await loadImage('https://s6.imgcdn.dev/ZqFsH.png');
+    const gradientImage = await loadImage("https://s6.imgcdn.dev/9Qs3v.png");
     ctx.drawImage(gradientImage, 0, 0, canvasWidth, canvasHeight);
-  
-  
+
     const avatar = await loadImage(this.avatar);
-  
-  
-    ctx.fillStyle = '#FFD700'; 
-ctx.font = '91px username';
-ctx.textAlign = 'center';
 
-ctx.fillText(`${this.username}`.toUpperCase(), centerX, centerY + 70);
+    ctx.fillStyle = "#FFD700";
+    ctx.font = "91px username";
+    ctx.textAlign = "center";
 
-ctx.fillStyle = '#FFFFFF'; 
-ctx.globalAlpha = 0.4;
+    ctx.fillText(`${this.username}`.toUpperCase(), centerX, centerY + 70);
 
-ctx.fillStyle = '#00BFFF';
-ctx.font = '76px welcome';
+    ctx.fillStyle = "#FFFFFF";
+    ctx.globalAlpha = 0.4;
 
-ctx.fillText(`${this.title}`, centerX, centerY + 150);
-ctx.fillStyle = '#FFFFFF'; 
-ctx.font = 'bold 41px montserrat-extra-light';
+    ctx.fillStyle = "#00BFFF";
+    ctx.font = "76px welcome";
 
-ctx.fillText(`${this.message}`.toUpperCase(), centerX, centerY + 290);
+    ctx.fillText(`${this.title}`, centerX, centerY + 150);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "bold 41px montserrat-extra-light";
 
-ctx.globalAlpha = 1;
+    ctx.fillText(`${this.message}`.toUpperCase(), centerX, centerY + 290);
 
-  
+    ctx.globalAlpha = 1;
+
     ctx.save();
     ctx.beginPath();
-    ctx.arc(510 + 130, 92 + 130, 130, 0, Math.PI * 2) 
+    ctx.arc(510 + 130, 92 + 130, 130, 0, Math.PI * 2);
     ctx.closePath();
     ctx.clip();
-    ctx.drawImage(avatar, 510, 92, 260, 260); 
+    ctx.drawImage(avatar, 510, 92, 260, 260);
     ctx.restore();
-  
 
     return canvas.toBuffer("image/png");
   }
