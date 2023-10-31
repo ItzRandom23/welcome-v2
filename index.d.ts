d45fd5ac8e646eef9101763f7e7dd2891d2e5eda
Mr Random <100831398+MrRandom23@users.noreply.github.com>
2023-10-31 20:53:03 +0530

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare class welcome {
+    constructor(options: {
+      username?: string;
+      avatar?: string;
+      title?: string;
+      message?: string;
+      background?: string;
+      usernameColor?: string;
+      titleColor?: string;
+      messageColor?: string;
+    });
+  
+    setName(name: string, usernameColor?: string): this;
+    setAvatar(image: string): this;
+    setTitle(title: string, titleColor?: string): this;
+    setMessage(message: string, messageColor?: string): this;
+    setBackground(background: string): this;
+    build(): Promise<Buffer>;
+}
+  
+  export = welcome;
+  
